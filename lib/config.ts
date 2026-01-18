98fafc46a420087ca5ae23e98cfc0d3e13400c4a
zain2983 <zainijaz2983@gmail.com>
2026-01-19 03:24:57 +0500

    fixed config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = 'http://localhost:8000'
-// export const BACKEND_URL = 'https://analyzr-backend.onrender.com'
+// export const BACKEND_URL = 'http://localhost:8000'
+export const BACKEND_URL = 'https://analyzr-backend.onrender.com'
